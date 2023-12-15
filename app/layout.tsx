4097ddb0ee02e14f9bf1457852f0949b59c4689f
wyfyjc <1819351418@qq.com>
2023-12-15 18:14:21 +0800

    Reload

---
+++ app/layout.tsx
@@ -0,0 +1,41 @@
+/* eslint-disable @next/next/no-page-custom-font */
+import "./styles/globals.scss";
+import "./styles/markdown.scss";
+import "./styles/highlight.scss";
+import { getClientConfig } from "./config/client";
+import { type Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Goldenglow ChatGPT",
+  description: "Goldenglow No.1!",
+  viewport: {
+    width: "device-width",
+    initialScale: 1,
+    maximumScale: 1,
+  },
+  themeColor: [
+    { media: "(prefers-color-scheme: light)", color: "#fdd2d3" },
+    { media: "(prefers-color-scheme: dark)", color: "#5c3f48" },
+  ],
+  appleWebApp: {
+    title: "Goldenglow ChatGPT",
+    statusBarStyle: "default",
+  },
+};
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="zh-cn">
+      <head>
+        <meta name="config" content={JSON.stringify(getClientConfig())} />
+        <link rel="manifest" href="/site.webmanifest"></link>
+        <script src="/serviceWorkerRegister.js" defer></script>
+      </head>
+      <body>{children}</body>
+    </html>
+  );
+}
