fa8214437211e3620f418a55667cd963c13ce775
wyfyjc <1819351418@qq.com>
2024-07-27 23:47:00 +0800

    fix

--- app/components/sidebar.tsx
+++ app/components/sidebar.tsx
@@ -222,8 +222,8 @@ export function SideBar(props: { className?: string }) {
       {...props}
     >
       <SideBarHeader
-        title="NextChat"
-        subTitle="Build your own AI assistant."
+        title="GG ChatGPT"
+        subTitle="Goldenglow No.1!"
         logo={<ChatGptIcon />}
       >
         <div className={styles["sidebar-header-bar"]}>
