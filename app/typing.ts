bda5f4d795478a388a654d027a69469ec928f49c
wyfyjc <1819351418@qq.com>
2024-07-27 23:32:40 +0800

    Clear repository

--- app/typing.ts
+++
@@ -1,9 +0,0 @@
-export type Updater<T> = (updater: (value: T) => void) => void;
-
-export const ROLES = ["system", "user", "assistant"] as const;
-export type MessageRole = (typeof ROLES)[number];
-
-export interface RequestMessage {
-  role: MessageRole;
-  content: string;
-}
