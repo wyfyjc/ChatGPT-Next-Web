9556009abc3a1c6e2fb74270bc338a9d1dcea76b
wyfyjc <1819351418@qq.com>
2024-12-28 19:43:19 +0800

    added new claude models

--- app/constant.ts
+++ app/constant.ts
@@ -254,6 +254,9 @@ const anthropicModels = [
   "claude-3-opus-20240229",
   "claude-3-haiku-20240307",
   "claude-3-5-sonnet-20240620",
+  "claude-3-5-haiku-20241022",
+  "claude-3-5-sonnet-20241022",
+  "claude-3-5-sonnet-latest",
 ];
 
 const baiduModels = [
