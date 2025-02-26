d2894f10c18836749481f4d4d9e30520b87d6c60
wyfyjc <1819351418@qq.com>
2025-02-26 22:30:07 +0800

    Update

--- app/constant.ts
+++ app/constant.ts
@@ -164,7 +164,7 @@ export const Stability = {
 };
 
 export const Anthropic = {
-  ChatPath: "v1",
+  ChatPath: "v1/chat/completions",
   ChatPath1: "v1/complete",
   ExampleEndpoint: "https://api.anthropic.com",
   Vision: "2023-06-01",
